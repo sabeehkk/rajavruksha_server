6c23d471009783ee46d2031642b19e8199d48cd4
Monica <monica.b.raju001@gmail.com>
2024-11-20 12:56:12 +0530

    fixed server issue

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ mongoose.connect(cluster_url)
     .then(() => console.log("Database connected"))
     .catch((err) => console.log(err));
 
-    
+
+
 // Middleware
 app.use(cors({
   // origin: 'http://localhost:3038'
